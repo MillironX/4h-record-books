313c034372e7079335439f6796b590455fe4a278
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2019-07-25 17:17:19 -0600

    Added all front-end pages for all sections

--- script/site.js
+++ script/site.js
@@ -1,13 +1,13 @@
 // JavaScript Document
 // From https://css-tricks.com/text-blocks-over-image/
 
-$(function() {
-	'use strict';
-	
-	$("h6")
-		.wrapInner("<span>");
-	
-	$("h6 br")
-		.before("<span class='spacer'>")
-		.after("<span class='spacer'>");
-});
+//$(function() {
+//	'use strict';
+//	
+//	$("h6")
+//		.wrapInner("<span>");
+//	
+//	$("h6 br")
+//		.before("<span class='spacer'>")
+//		.after("<span class='spacer'>");
+//});
